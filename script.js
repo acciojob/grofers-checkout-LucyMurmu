234ddf7f72310337bdd25da57e2a40c8072e33bd
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-06-02 03:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const prices=document.querySelectorAll(".prices");
+	let total=0;
+	prices.forEach(prices =>{
+		total+=Number(prices.textContent);
+	});
+	const row=document.createElement("tr");
+	const cell=document.createElement("td");
+
+	cell.colspan=2;
+	cell.textContent=total;
+	row.appendChild(cell);
+	document.querySelector("table").appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
